baf11d81a61d91eb861aef44de63f7e37e68d8f2
Corentin <Corentinfrdr@gmail.com>
2025-04-09 11:34:01 +0200

    Mise à jour des classes MesDates et MesDatesTest, et du composer.json

--- tests/MesDatesTest.php
+++ tests/MesDatesTest.php
@@ -1,36 +1,34 @@
 <?php
 
-namespace Tests;
+namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
 use App\MesDates;
-
-// Importez la classe que vous voulez tester
 
 class MesDatesTest extends TestCase
 {
     public function testDateDemainRetourneJsonEtDateCorrecte()
     {
-        // Instanciez la classe que vous voulez tester
-        $mesDates = new MesDates();
+        $mesDates = new MesDates('2025-04-09'); // La date ici n'est pas directement testée pour "demain"
 
-        // Appelez la méthode que vous voulez tester
         $resultat = $mesDates->getDateDemainEnJson();
 
-        // Assurez-vous que le résultat est une chaîne de caractères (JSON)
         $this->assertIsString($resultat);
 
-        // Décodez le JSON pour vérifier son contenu
         $data = json_decode($resultat, true);
 
-        // Assurez-vous que le décodage a réussi et que c'est un tableau associatif
         $this->assertIsArray($data);
         $this->assertArrayHasKey('demain', $data);
 
-        // Obtenez la date de demain attendue au format Y-m-d
-        $demainAttendu = (new DateTime('tomorrow'))->format('Y-m-d');
+        $demainAttendu = (new \DateTime('tomorrow'))->format('Y-m-d');
 
-        // Assurez-vous que la valeur de 'demain' dans le JSON correspond à la date de demain attendue
         $this->assertEquals($demainAttendu, $data['demain']);
+    }
+
+    public function testGetDateRetourneLaDateInitialisee()
+    {
+        $dateInitiale = '2025-04-09';
+        $mesDates = new MesDates($dateInitiale);
+        $this->assertEquals($dateInitiale, $mesDates->getDate());
     }
 }
