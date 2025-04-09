a96cdb54be14892ef69e1e14e039f51c4a5085ff
Corentin <Corentinfrdr@gmail.com>
2025-04-09 10:51:45 +0200

    Add PHP_CodeSniffer dependency

--- src/MesDates.php
+++ src/MesDates.php
@@ -1,12 +1,30 @@
 <?php
 
-namespace App; // Ajoutez un namespace si vous en utilisez un
-
+// Classe MesDates pour la gestion des dates
 class MesDates
 {
-    public function getDateDemainEnJson(): string
+    // Déclaration des propriétés
+    private $date;
+
+    // Constructeur de la classe
+    public function __construct($date)
     {
-        $demain = (new \DateTime('tomorrow'))->format('Y-m-d');
-        return json_encode(['demain' => $demain]);
+        $this->date = $date;
+    }
+
+    // Méthode pour obtenir la date
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    // Méthode pour afficher la date
+    public function afficherDate()
+    {
+        echo $this->date;
     }
 }
+
+// Exemple d'utilisation de la classe
+$maDate = new MesDates('2025-04-09');
+$maDate->afficherDate();
